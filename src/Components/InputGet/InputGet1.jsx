71a95d53a6c80f5e3699c2b039aa61c159a2163e
Mr-ankit-raj <59471441+Mr-ankit-raj@users.noreply.github.com>
2023-02-11 08:21:16 +0530

    inpur get

--- src/Components/InputGet/InputGet1.jsx
+++ src/Components/InputGet/InputGet1.jsx
@@ -10,7 +10,7 @@ const InputGet = () => {
       <div className="mainCardDown">
         {print ? <h1>{data}</h1> : null}
         <div>
-          <input type="text" onChange={getData} />
+          <input type="tex" onChange={getData} />
           <button onClick={() => setprint(true)}> Print Data</button>
         </div>
       </div>
