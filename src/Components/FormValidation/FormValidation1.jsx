2fb58d805ecb75f0dd9410d798c82b03ad3911ee
Mr-ankit-raj <59471441+Mr-ankit-raj@users.noreply.github.com>
2023-02-17 19:41:35 +0530

    Form Validations 1

--- src/Components/FormValidation/FormValidation1.jsx
+++ src/Components/FormValidation/FormValidation1.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const FormValidation1 = () => {
+  const [user, setUser] = useState();
+  const [pass, setPass] = useState();
+  const [userErr, setUserErr] = useState(false);
+  const [passErr, setpassErr] = useState(false);
   return (
     <div className="mainCard">
-      <h1>Hi</h1>
+      <form onSubmit={GetData}>
+        <h1>Hi</h1>
+        <input type="text" onChange={userHandle} />
+        {userErr ? <span>User Invalid</span> : ""}
+        <input type="text" onChange={passHandle} />
+        {passErr ? <span>Password Invalid</span> : ""}
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
