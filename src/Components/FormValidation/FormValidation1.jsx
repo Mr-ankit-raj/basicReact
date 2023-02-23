16a0348d5ca3bb7b5a125ed11fa6d97c6e2ccf1d
Mr-ankit-raj <59471441+Mr-ankit-raj@users.noreply.github.com>
2023-02-23 12:16:07 +0530

    validations

--- src/Components/FormValidation/FormValidation1.jsx
+++ src/Components/FormValidation/FormValidation1.jsx
@@ -6,23 +6,47 @@ const FormValidation1 = () => {
   const [userErr, setUserErr] = useState(false);
   const [passErr, setpassErr] = useState(false);
   function GetData(e) {
+    if (user.lenght > 3 || pass.lenght > 3) {
+      alert("invalid");
+    } else {
+      alert("all Goods :)");
+    }
     e.preventDefault();
   }
+  function userHandle(e) {
+    let item = e.target.value;
+    if (item.lenght < 3) {
+      userErr(true);
+    } else {
+      userErr(false);
+    }
+    setUser(item);
+  }
+  function passHandle(e) {
+    let item = e.target.value;
+    if (item.lenght < 2) {
+      passErr(true);
+    } else {
+      passErr(false);
+    }
+    setPass(item);
+  }
+
   return (
     <div className="mainCard">
       <form onSubmit={GetData}>
-        <h1>Hi</h1>
+        <h1>Form Validations 2</h1>
         <input type="text" onChange={userHandle} />
-        {userErr ? (
-          <span>
+        {setUserErr ? (
+          <span class="error">
             <p>User Invalid</p>
           </span>
         ) : (
           ""
         )}
         <input type="text" onChange={passHandle} />
-        {passErr ? (
-          <span>
+        {setpassErr ? (
+          <span class="error">
             <p>Password Invalid</p>
           </span>
         ) : (
